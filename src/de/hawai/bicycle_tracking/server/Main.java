061fc717450a3095cc088cc8d3f2e28403843012
TorbenHaug <claustorbenhaug@hotmail.de>
2015-03-23 13:48:03 +0100

    Auto format

--- src/de/hawai/bicycle_tracking/server/Main.java
+++ src/de/hawai/bicycle_tracking/server/Main.java
@@ -1,9 +1,7 @@
 package de.hawai.bicycle_tracking.server;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        
-    }
+public class Main {
+	public static void main(String[] args) {
+
+	}
 }
