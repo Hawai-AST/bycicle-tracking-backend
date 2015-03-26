7193b30ea8d59abd733ca7c8dc61eb5042314e8d
Tim Hagemann <tim@infinityblade.de>
2015-03-26 16:38:58 +0100

    Made Server runnable

--- src/de/hawai/bicycle_tracking/server/Main.java
+++ src/de/hawai/bicycle_tracking/server/Main.java
@@ -1,7 +1,10 @@
 package de.hawai.bicycle_tracking.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
 public class Main {
 	public static void main(String[] args) {
-
+		SpringApplication.run(Main.class, args);
 	}
 }
