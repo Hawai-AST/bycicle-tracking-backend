99e3a4b60287f8453ad1cff0f465c868d9bc8f1a
Tim Hagemann <kumpelblase2@users.noreply.github.com>
2015-04-04 19:42:47 +0200

    Fix Main not running.

--- src/de/hawai/bicycle_tracking/server/Main.java
+++ src/de/hawai/bicycle_tracking/server/Main.java
@@ -1,7 +1,15 @@
 package de.hawai.bicycle_tracking.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Main {
 	public static void main(String[] args) {
-
+		SpringApplication.run(Main.class, args);
 	}
 }
