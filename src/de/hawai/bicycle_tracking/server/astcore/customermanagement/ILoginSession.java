34b131352fc4787c7054a88d994d850c59e1b8cf
Tim Hagemann <tim@infinityblade.de>
2015-04-04 19:43:49 +0200

    Add login session entity.

---
+++ src/de/hawai/bicycle_tracking/server/astcore/customermanagement/ILoginSession.java
@@ -0,0 +1,10 @@
+package de.hawai.bicycle_tracking.server.astcore.customermanagement;
+
+public interface ILoginSession
+{
+	IApplication getApplication();
+
+	IUser getUser();
+
+	String getToken();
+}
