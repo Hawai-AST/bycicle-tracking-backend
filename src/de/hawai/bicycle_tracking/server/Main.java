54e1924d645e10ebc549899759d74f3d02aa6df4
Tim Hagemann <tim@infinityblade.de>
2015-04-07 14:31:03 +0200

    Make Main runnable.

--- src/de/hawai/bicycle_tracking/server/Main.java
+++ src/de/hawai/bicycle_tracking/server/Main.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableWebMvc
+@Configuration
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
