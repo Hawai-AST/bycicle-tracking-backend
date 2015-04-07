2a73da511a246ec0d777f781ce15f8d690ba8ab9
Tim Hagemann <tim@infinityblade.de>
2015-04-07 14:12:56 +0200

    Cleanup imports.

--- src/de/hawai/bicycle_tracking/server/AppConfig.java
+++ src/de/hawai/bicycle_tracking/server/AppConfig.java
@@ -1,8 +1,6 @@
 package de.hawai.bicycle_tracking.server;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
