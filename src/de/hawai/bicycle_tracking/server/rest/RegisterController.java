3868009ae0537a28b7bafd6b88b56cca0b6cc783
fap <fap@gaia.gaia>
2015-04-09 16:05:14 +0200

    Setter in User, Address and EMail are private now.

--- src/de/hawai/bicycle_tracking/server/rest/RegisterController.java
+++ src/de/hawai/bicycle_tracking/server/rest/RegisterController.java
@@ -22,8 +22,7 @@ import de.hawai.bicycle_tracking.server.dto.RegistrationDTO;
 import de.hawai.bicycle_tracking.server.facade.Facade;
 import de.hawai.bicycle_tracking.server.rest.exceptions.AlreadyExistsException;
 import de.hawai.bicycle_tracking.server.rest.exceptions.InvalidClientException;
-import org.springframework.web.bind.annotation.*;
-@RestController
+import org.springframework.web.bind.annotation.*;@RestController
 @RequestMapping("/api")
 public class RegisterController {
 	private final DateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd");
