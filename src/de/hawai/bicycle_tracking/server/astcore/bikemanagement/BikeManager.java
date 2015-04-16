274ee093a3e1b9bf3d6fafa18a8c23cb4f5a9fbb
fap <fap@gaia.gaia>
2015-04-16 23:06:53 +0200

    Adding IBikeManager

--- src/de/hawai/bicycle_tracking/server/astcore/bikemanagement/BikeManager.java
+++ src/de/hawai/bicycle_tracking/server/astcore/bikemanagement/BikeManager.java
@@ -8,15 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import de.hawai.bicycle_tracking.server.astcore.customermanagement.User;
 import de.hawai.bicycle_tracking.server.utility.value.FrameNumber;
 
-public class BikeManager {
+public class BikeManager implements IBikeManager {
 
 	@Autowired
 	private BikeDao bikeDao;
 
+	@Override
 	public List<? extends IBike> findBikesBySoldLocation(SellingLocation inSellingLocation) {
 		return bikeDao.findBySoldLocation(inSellingLocation);
 	}
 
+	@Override
 	public IBike createBike(String inType, FrameNumber inFrameNumber, Date inBuyDate,
 			Date inNextMaintenanceDate, SellingLocation inSellingLocation, User inOwner) {
 		return bikeDao.save(new Bike(inType, inFrameNumber, inBuyDate, inNextMaintenanceDate, inSellingLocation, inOwner));
