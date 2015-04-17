a3d9c967783212eb46e8399b8bcee083786b8055
Tim Hagemann <tim.hagemann@haw-hamburg.de>
2015-04-17 17:12:11 +0200

    Create own package.

---
+++ src/de/hawai/bicycle_tracking/server/security/OAuth2Config.java
@@ -0,0 +1,79 @@
+package de.hawai.bicycle_tracking.server.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter
+{
+	private static final int VALIDITY_TIME = 60 * 60 * 24 * 7;
+	private static final String RESOURCE_ID = "hawai-ast";
+
+	@Autowired
+	private AuthenticationManager m_authenticationManager;
+
+	@Autowired
+	private UserDetailsService detailsService;
+
+	private TokenStore tokenStore = new InMemoryTokenStore();
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception
+	{
+		endpoints.authenticationManager(m_authenticationManager).tokenStore(tokenStore).userDetailsService(detailsService);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+			.inMemory()
+				.withClient("DEV-101")
+					.authorizedGrantTypes("password", "refresh_token")
+					.authorities("USER", "ADMIN")
+					.scopes("read", "write")
+					.resourceIds(RESOURCE_ID)
+					.accessTokenValiditySeconds(VALIDITY_TIME);
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices tokenServices = new DefaultTokenServices();
+		tokenServices.setSupportRefreshToken(true);
+		tokenServices.setTokenStore(this.tokenStore);
+		return tokenServices;
+	}
+
+	@Configuration
+	@EnableResourceServer
+	public static class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+			resources.resourceId(RESOURCE_ID);
+		}
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+					.antMatchers("/v{\\d+}/register", "/v{\\d+}/login").permitAll()
+					.antMatchers("/v{\\d+}/user/*").hasRole("ADMIN");
+
+		}
+	}
+}
