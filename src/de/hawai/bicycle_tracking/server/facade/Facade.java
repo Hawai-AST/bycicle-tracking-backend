1e4d4c6163eaaaf2bc46d622b6820cd1a555b031
Tim Hagemann <tim.hagemann@haw-hamburg.de>
2015-04-19 14:36:37 +0200

    Add facade methods for bikemanagement

--- src/de/hawai/bicycle_tracking/server/facade/Facade.java
+++ src/de/hawai/bicycle_tracking/server/facade/Facade.java
@@ -1,21 +1,28 @@
 package de.hawai.bicycle_tracking.server.facade;
 
-import java.util.Date;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
+import de.hawai.bicycle_tracking.server.astcore.bikemanagement.IBike;
+import de.hawai.bicycle_tracking.server.astcore.bikemanagement.IBikeManager;
+import de.hawai.bicycle_tracking.server.astcore.bikemanagement.ISellingLocation;
 import de.hawai.bicycle_tracking.server.astcore.customermanagement.ICustomerManagement;
 import de.hawai.bicycle_tracking.server.astcore.customermanagement.IUser;
 import de.hawai.bicycle_tracking.server.utility.value.Address;
 import de.hawai.bicycle_tracking.server.utility.value.EMail;
+import de.hawai.bicycle_tracking.server.utility.value.FrameNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class Facade {
 
 	@Autowired
 	private ICustomerManagement customerManegement;
+
+	@Autowired
+	private IBikeManager bikeManagement;
 
 	private Facade() {
 		super();
@@ -29,4 +36,15 @@ public class Facade {
 		return customerManegement.registerUser(name, firstName, eMailAddress, address, birthdate, password);
 	}
 
+	public List<? extends IBike> findBikesBySoldLocation(ISellingLocation inLocation) {
+		return this.bikeManagement.findBikesBySoldLocation(inLocation);
+	}
+
+	public IBike createBike(String inType, FrameNumber inFrameNumber, Date inBuyDate, Date inNextMaintenanceDate, ISellingLocation inSellingLocation, IUser inOwner) {
+		return this.bikeManagement.createBike(inType, inFrameNumber, inBuyDate, inNextMaintenanceDate, inSellingLocation, inOwner);
+	}
+
+	public List<? extends IBike> findByOwner(IUser inOwner) {
+		return this.bikeManagement.findByOwner(inOwner);
+	}
 }
