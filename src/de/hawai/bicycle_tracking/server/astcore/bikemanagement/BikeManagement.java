3e071d2de41b126409201015da30b09071f880d9
TorbenHaug <claustorbenhaug@hotmail.de>
2015-05-14 13:01:55 +0200

    #AST-100 Remove Duplicate Import

--- src/de/hawai/bicycle_tracking/server/astcore/bikemanagement/BikeManagement.java
+++ src/de/hawai/bicycle_tracking/server/astcore/bikemanagement/BikeManagement.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Component;
 import de.hawai.bicycle_tracking.server.astcore.customermanagement.IUser;
 import de.hawai.bicycle_tracking.server.utility.value.Address;
 import de.hawai.bicycle_tracking.server.utility.value.FrameNumber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
